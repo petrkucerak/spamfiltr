6163979ebd8273bc471147d0b3d854a0e947f90d
petrkucerak <45851215+petrkucerak@users.noreply.github.com>
2021-01-05 00:05:24 +0100

    add report file

--- macros.py
+++
@@ -1,8 +0,0 @@
-class CustomMacros:
-    standard_email_address = "standardEmailAddress"
-    standard_pure_number = "standardPureNumber"
-    standard_phone_number = "standardPhoneNumber"
-    standard_url = "standardUrlString"
-    standard_ip_address = "standardIpAddress"
-    standard_price_string = "standardPriceString"
-    notable_repeating_sequence = "notableRepeatingChars"
